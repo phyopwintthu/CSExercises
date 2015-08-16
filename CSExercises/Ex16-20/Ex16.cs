9018b631986150c3c345b55042b96618e95f57e5
phyopwintthu <e0000709@u.nus.edu>
2015-08-16 23:31:38 +0800

    Done Exercise 16- 19

--- CSExercises/Ex16-20/Ex16.cs
+++ CSExercises/Ex16-20/Ex16.cs
@@ -1,10 +1,27 @@
+using System;
 namespace CSExercises
 {
     public class Ex16
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Plese Enter the name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Plese Enter the gender(M or F)");
+            string gender = Console.ReadLine();
+
+            if(gender == "F")
+            {
+                Console.WriteLine("Ms. {0}",name);
+            }
+            else if (gender == "M")
+            {
+                Console.WriteLine("Mr. {0}", name);
+            }
+            else
+            {
+                Console.WriteLine("Do not match with request.");
+            }
         }
     }
 }
--- CSExercises/Ex16-20/Ex17.cs
+++ CSExercises/Ex16-20/Ex17.cs
@@ -1,10 +1,37 @@
+using System;
 namespace CSExercises
 {
     public class Ex17
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Plese Enter the name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Plese Enter the gender(M or F)");
+            string gender = Console.ReadLine();
+            Console.WriteLine("Plese Enter the age");
+            int age = Convert.ToInt16(Console.ReadLine());
+
+            if (gender == "M" && age >= 40)
+            {
+                Console.WriteLine("Good Morning Uncle {0}", name);
+            }
+            else if (gender == "F" && age >= 40)
+            {
+                Console.WriteLine("Good Morning Aunty {0}", name);
+            }
+            else if (gender == "F")
+            {
+                Console.WriteLine("Good Morning Ms.{0}", name);
+            }
+            else if (gender == "M")
+            {
+                Console.WriteLine("Good Morning Mr.{0}", name);
+            }
+            else
+            {
+                Console.WriteLine("Do not match with request.");
+            }
         }
     }
 }
