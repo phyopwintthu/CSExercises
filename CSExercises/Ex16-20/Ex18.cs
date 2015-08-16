9ba7304fb475512fec4dee045fdfed56aa1eefd3
phyopwintthu <e0000709@u.nus.edu>
2015-08-16 23:34:17 +0800

    Revert "Done Exercise 16- 19"
    This reverts commit 9018b631986150c3c345b55042b96618e95f57e5.

--- CSExercises/Ex16-20/Ex18.cs
+++ CSExercises/Ex16-20/Ex18.cs
@@ -1,33 +1,10 @@
-using System;
 namespace CSExercises
 {
     public class Ex18
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Plese Enter the marks");
-            int marks = Convert.ToInt16(Console.ReadLine());
-
-            if (marks < 0 || marks > 100)
-            {
-                Console.WriteLine("Incorrect Marks");
-            }
-            else if (marks >= 80 && marks <= 100)
-            {
-                Console.WriteLine("Grade A");
-            }
-            else if (marks >= 60 && marks <= 79)
-            {
-                Console.WriteLine("Grade B");
-            }
-            else if (marks >= 41 && marks <= 59)
-            {
-                Console.WriteLine("Grade C");
-            }
-            else if (marks >= 0 && marks <= 40)
-            {
-                Console.WriteLine("Fail");
-            }      
+            
         }
     }
 }
--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -1,34 +1,10 @@
-using System;
 namespace CSExercises
 {
     public class Ex19
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Plese Enter the distance in Kilometers");
-            double dis_kilo = Convert.ToDouble(Console.ReadLine());
-            double dis_meters = dis_kilo * 1000; 
-
-            if (dis_meters <= 500 )
-            {
-                double fare_cost = 2.4;
-                Console.WriteLine("Fare Cost ${0}", fare_cost);
-            }
-            else if (dis_meters >= 501 && dis_meters <= 850) {
-                double fare_cost = 2.4 + (dis_meters/10)*0.04;
-                Console.WriteLine("Fare Cost ${0}", fare_cost);
-            }
-            else if (dis_meters >= 851)
-            {
-                //double fare_cost1 = 2.4 + (dis_meters / 10) * 0.04;
-                //double fare_cost2 = 2.4 + ((dis_meters-850)/10) * 0.05;
-                //double fare_cost = fare_cost1 + fare_cost2;
-               // Console.WriteLine("Fare Cost ${0}", fare_cost);
-            }
-            else
-            {
-                Console.WriteLine("Errors");
-            }
+            
         }
     }
 }
